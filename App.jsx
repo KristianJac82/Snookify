454d4664d0c5a59dc8eec401adabae03ee8d2206
KristianJac82 <kristian@zentro.se>
2025-05-04 21:19:52 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Snookify!</h1>
+      <p>Upload your image and start your personalization journey.</p>
+    </div>
+  )
+}
+
+export default App
